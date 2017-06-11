aa0214dcdce67c4f5342f27f22468de1fd545235
abhinavsukith <sukithabhinav62@gmail.com>
2017-06-10 17:47:47 -0700

    added client, server modules

---
+++ socket_programming/client.h
@@ -0,0 +1,14 @@
+//
+//  client.h
+//  socket_programming
+//
+//  Created by abhinavsukith on 11/28/16.
+//  Copyright © 2016 User. All rights reserved.
+//
+
+#ifndef client_h
+#define client_h
+
+#include <stdio.h>
+
+#endif /* client_h */
---
+++ socket_programming/server.h
@@ -0,0 +1,14 @@
+//
+//  server.h
+//  socket_programming
+//
+//  Created by abhinavsukith on 11/28/16.
+//  Copyright © 2016 User. All rights reserved.
+//
+
+#ifndef server_h
+#define server_h
+
+#include <stdio.h>
+
+#endif /* server_h */
